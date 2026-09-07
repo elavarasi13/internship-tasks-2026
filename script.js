3e1843d15bdfdafd069dbad185dbad2b1affbe46
elavarasi13 <elavarasie827@gmail.com>
2026-09-07 09:48:48 +0530

    Task 3

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-let currentImage = 0;
+// Simple welcome message
 
-let images = [
-    "mountain.jpg",
-    "beach.jpg",
-    "flower.jpg",
-    "sunset.jpg",
-    "forest.jpg",
-    "waterfall.jpg"
-];
+window.addEventListener("load", function() {
 
-function openImage(index) {
-    currentImage = index;
-    document.getElementById("lightbox").style.display = "flex";
-    document.getElementById("lightbox-img").src = images[currentImage];
-}
+    console.log("Welcome to My Portfolio!");
 
-function closeImage() {
-    document.getElementById("lightbox").style.display = "none";
-}
-
-function nextImage() {
-    currentImage = (currentImage + 1) % images.length;
-    document.getElementById("lightbox-img").src = images[currentImage];
-}
-
-function prevImage() {
-    currentImage = (currentImage - 1 + images.length) % images.length;
-    document.getElementById("lightbox-img").src = images[currentImage];
-}
+});
